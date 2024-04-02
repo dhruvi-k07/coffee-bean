3af91a8cb4931e6ad0f0b84bea9aef1efaef9e18
Dhruvi Kunvarani <dhruvik@socialpilot.co>
2024-04-02 17:29:30 -0400

    Bean Classification Done

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import numpy as np
+import cv2
 import os
 from flask_cors import CORS
 
@@ -17,15 +17,16 @@ model = load_model(MODEL_PATH)
 CATEGORIES = ['Dark', 'Green', 'Light', 'Medium']
 
 def model_predict(img_path, model):
-    IMG_SIZE = 50  # Target size
+    IMG_SIZE = 50  # Target size for the model
     try:
-        # Load and resize the image
-        img = image.load_img(img_path, target_size=(IMG_SIZE, IMG_SIZE))
-        x = image.img_to_array(img)
-        # Normalize the image data
-        x = x / 255.0
+        # Load the image using OpenCV
+        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+        # Resize the image
+        resized_img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        # Normalize the image data to 0-1
+        normalized_img = resized_img / 255.0
         # Reshape the image data for the model
-        x = np.expand_dims(x, axis=0)
+        x = np.expand_dims(normalized_img, axis=0)
         preds = model.predict(x)
         return preds, None
     except Exception as e:
@@ -33,7 +34,6 @@ def model_predict(img_path, model):
 
 @app.route('/')
 def index():
-    # Assuming you have an 'index.html' that contains the UI for uploading images
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
@@ -57,7 +57,7 @@ def predict():
         return jsonify({"error": f"Prediction failed: {error}"}), 500
 
     # Process the prediction to get the category with the highest probability
-    prediction_index = np.argmax(preds, axis=1)[0]  # Getting the index of the max probability
+    prediction_index = np.argmax(preds, axis=1)[0]
     result = CATEGORIES[prediction_index]
 
     return jsonify({"prediction": result})
